283038b333b41c136ea63034bb5d45431b9805bf
khizer08 <syedkhizer2004@gmail.com>
2026-01-15 22:08:07 +0530

    pending

--- striver-patterns/seventh.java
+++ striver-patterns/seventh.java
@@ -1,3 +1,14 @@
 public class seventh{
-    
+
+    public static void triangle(int n){
+        for(int i=n;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+        }
+    }
+    public static void main(String[] args) {
+        triangle(5);
+    }
 }
