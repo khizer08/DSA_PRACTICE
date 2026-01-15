076c440efc99d027c38dbbdd76dbf2c24536025f
khizer08 <syedkhizer2004@gmail.com>
2026-01-15 21:18:30 +0530

    square-pattern

---
+++ striver-patterns/first.java
@@ -0,0 +1,13 @@
+public class first {
+    public static void pattern1(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        pattern1(4);
+    }
+}
