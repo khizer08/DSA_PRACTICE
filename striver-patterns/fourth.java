a2226eb5b3826160bc3f5a74abb3b8817928de13
khizer08 <syedkhizer2004@gmail.com>
2026-01-15 21:30:12 +0530

    ContinousRightAngledNumPattern

--- striver-patterns/fourth.java
+++ striver-patterns/fourth.java
@@ -1,6 +1,6 @@
 public class fourth{
 
-    public static void ReverseRightAngledNumPattern(int n){
+    public static void ContinousRightAngledNumPattern(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(i);
@@ -9,7 +9,7 @@ public class fourth{
         }
     }
     public static void main(String[] args) {
-        ReverseRightAngledNumPattern(5);
+        ContinousRightAngledNumPattern(5);
     }
 }
 
