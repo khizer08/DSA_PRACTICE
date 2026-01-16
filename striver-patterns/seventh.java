cc670014acf1cfac48c74a0134b51099989af380
khizer08 <syedkhizer2004@gmail.com>
2026-01-16 22:05:30 +0530

    hollowsquare

--- striver-patterns/seventh.java
+++ striver-patterns/seventh.java
@@ -3,9 +3,9 @@ public class seventh{
     public static void triangle(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n*2-1;j++){
-                System.out.print(" ");
+                System.out.print("1");
             }
-            System.out.print("*");
+            System.out.println("*");
         }
     }
     public static void main(String[] args) {
