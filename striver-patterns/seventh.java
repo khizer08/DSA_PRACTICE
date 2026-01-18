9ffe95a74fc12c2d78b9ee5092ea68e106bd73bd
khizer08 <syedkhizer2004@gmail.com>
2026-01-18 21:08:48 +0530

    triangle-pattern

--- striver-patterns/seventh.java
+++ striver-patterns/seventh.java
@@ -1,14 +1,32 @@
 public class seventh{
 
     public static void triangle(int n){
+
+        // number of lines
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n*2-1;j++){
-                System.out.print("1");
+            //initial spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
             }
-            System.out.println("*");
+            //stars
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            //final spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
         triangle(5);
     }
 }
+
+// output:-
+//     *    
+//    ***
+//   *****
+//  *******
+// *********
