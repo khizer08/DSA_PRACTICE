44f23f52c6434563db658f95dc314412f3fbb02b
khizer08 <syedkhizer2004@gmail.com>
2026-01-22 20:14:31 +0530

    alphabet_triangle

---
+++ striver-patterns/fifteen.java
@@ -0,0 +1,23 @@
+public class fifteen{
+    public static void alphabet_triangle(int n){
+        char ch='A';// do remember "single quotes".
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            ch='A';
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        alphabet_triangle(5);
+    }
+}
+
+// output:-
+// ABCDE
+// ABCD
+// ABC
+// AB
+// A
