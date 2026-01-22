1a758a2907c1c081881d3b59ea8c21bfa354698e
khizer08 <syedkhizer2004@gmail.com>
2026-01-22 20:02:03 +0530

    continous_num_triangle

---
+++ striver-patterns/thirdteen.java
@@ -0,0 +1,23 @@
+public class thirdteen{
+    public static void continous_num_triangle(int n){
+        int k=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        continous_num_triangle(5);
+    }
+    
+}
+
+// output:-
+// 1 
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
