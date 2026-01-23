1aaf131056676b2d086609c8e908fb959737c305
khizer08 <syedkhizer2004@gmail.com>
2026-01-23 13:13:02 +0530

    Pending

---
+++ striver-patterns/twentysecond.java
@@ -0,0 +1,14 @@
+public class twentysecond{
+   public static void main(String[] args) {
+    // come back and solve again.
+   }
+}
+
+// output:-
+// 4 4 4 4 4 4 4 
+// 4 3 3 3 3 3 4
+// 4 3 2 2 2 3 4
+// 4 3 2 1 2 3 4
+// 4 3 2 2 2 3 4
+// 4 3 3 3 3 3 4
+// 4 4 4 4 4 4 4
